c59379a1ca0f81291ab36130c8fe6ce1110476e9
nazy67 <nazykh67@gmail.com>
2021-06-06 22:19:49 -0500

    updated vpc_secondary.tf

--- vpc_secondary.tf
+++ vpc_secondary.tf
@@ -7,13 +7,13 @@ resource "aws_vpc" "my_vpc_two" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_vpc"
+      Name = "${var.env}_secondary_vpc"
     }
   )
 }
 
 # Public Subnets
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "second_public_subnet" {
   count             = length(var.second_subnet_azs)
   vpc_id            = aws_vpc.my_vpc_two.id
   cidr_block        = element(var.second_pub_cidr_subnet, count.index)
@@ -21,13 +21,13 @@ resource "aws_subnet" "public_subnet" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_pub_sub_${count.index}"
+      Name = "${var.env}_secondary_pub_sub_${count.index}"
     }
   )
 }
 
 # Private Subnets
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "second_private_subnet" {
   count             = length(var.second_subnet_azs)
   vpc_id            = aws_vpc.my_vpc_two.id
   cidr_block        = element(var.second_priv_cidr_subnet, count.index)
@@ -35,91 +35,91 @@ resource "aws_subnet" "private_subnet" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_priv_sub_${count.index}"
+      Name = "${var.env}_secondary_priv_sub_${count.index}"
     }
   )
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "second_igw" {
   vpc_id = aws_vpc.my_vpc_two.id
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_igw"
+      Name = "${var.env}_secondary_igw"
     }
   )
 }
 
 # Public Route Table
-resource "aws_route_table" "pub_rtb" {
+resource "aws_route_table" "second_pub_rtb" {
   vpc_id = aws_vpc.my_vpc_two.id
 
   route {
     cidr_block = var.rt_cidr_block
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.second_igw.id
   }
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_pub_rtb"
+      Name = "${var.env}_secondary_pub_rtb"
     }
   )
 }
 
 # Public Route Table Association
-resource "aws_route_table_association" "pub_subnets" {
+resource "aws_route_table_association" "second_pub_subnets" {
   count = length(var.second_subnet_azs)
 
-  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
-  route_table_id = element(aws_route_table.pub_rtb.*.id, count.index)
+  subnet_id      = element(aws_subnet.second_public_subnet.*.id, count.index)
+  route_table_id = element(aws_route_table.second_pub_rtb.*.id, count.index)
 }
 
 # Elastic IP
-resource "aws_eip" "nat_gw_eip" {
+resource "aws_eip" "second_nat_gw_eip" {
   vpc = true
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_eip"
+      Name = "${var.env}_secondary_eip"
     }
   )
 }
 
 # Nat Gateway
-resource "aws_nat_gateway" "nat_gw" {
-  depends_on    = [aws_internet_gateway.igw]
-  allocation_id = aws_eip.nat_gw_eip.id
-  subnet_id     = aws_subnet.public_subnet[0].id
+resource "aws_nat_gateway" "second_nat_gw" {
+  depends_on    = [aws_internet_gateway.second_igw]
+  allocation_id = aws_eip.second_nat_gw_eip.id
+  subnet_id     = aws_subnet.second_public_subnet[0].id
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_nat_gw"
+      Name = "${var.env}_secondary_nat_gw"
     }
   )
 }
 
 # Private Route Table
-resource "aws_route_table" "private_rtb" {
+resource "aws_route_table" "second_private_rtb" {
   vpc_id = aws_vpc.my_vpc_two.id
 
   route {
     cidr_block     = var.rt_cidr_block
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
+    nat_gateway_id = aws_nat_gateway.second_nat_gw.id
   }
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.env}_private_rtb"
+      Name = "${var.env}_secondary_private_rtb"
     }
   )
 }
 
 # Private Route Table Association
-resource "aws_route_table_association" "priv_subnets" {
+resource "aws_route_table_association" "second_priv_subnets" {
   count = length(var.second_subnet_azs)
 
-  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id = element(aws_route_table.private_rtb.*.id, count.index)
+  subnet_id      = element(aws_subnet.second_private_subnet.*.id, count.index)
+  route_table_id = element(aws_route_table.second_private_rtb.*.id, count.index)
 }
