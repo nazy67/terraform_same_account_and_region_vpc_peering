2801390bdb13e2d367458263c3c191d09494a47e
nazy67 <nazykh67@gmail.com>
2021-06-06 22:19:25 -0500

    modified  vpc_peering.tf

--- vpc_peering.tf
+++ vpc_peering.tf
@@ -1,28 +1,20 @@
-resource "aws_vpc_peering_connection" "my_peering_requester" {
-  peer_vpc_id   = aws_vpc.my_vpc_two.id
-  vpc_id        = aws_vpc.my_vpc_one.id
-  auto_accept   = true
+// resource "aws_vpc_peering_connection" "my_peering_requester" {
+//   peer_vpc_id   = aws_vpc.my_vpc_two.id
+//   vpc_id        = aws_vpc.my_vpc_one.id
+//   auto_accept   = true
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
+//   accepter {
+//     allow_remote_vpc_dns_resolution = true
+//   }
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
+//   requester {
+//     allow_remote_vpc_dns_resolution = true
+//   }
 
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.env}_vpc_peering"
-    }
-  )
-}
-
-// resource "aws_vpc" "my_vpc_one" {
-//   cidr_block = "10.0.0.0/16"
-// }
-
-// resource "aws_vpc" "my_vpc_two" {
-//   cidr_block = "10.1.0.0/16"
+//   tags = merge(
+//     local.common_tags,
+//     {
+//       Name = "${var.env}_vpc_peering"
+//     }
+//   )
 // }
